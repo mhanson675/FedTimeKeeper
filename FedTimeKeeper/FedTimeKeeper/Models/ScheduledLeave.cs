c60ff3acb64b4aa732b2305fbe6e715a6c6e2941
Scotty Dev <mhanson675@gmail.com>
2021-07-30 11:17:17 -0500

    Scheduled Leave Page Prototyped
    Prototype for the Scheduled Leave Page is in place. Working on formatting before moving on.

---
+++ FedTimeKeeper/FedTimeKeeper/Models/ScheduledLeave.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FedTimeKeeper.Models
+{
+    public enum LeaveType { Annual, Sick, Timeoff }
+
+    public class ScheduledLeave
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double HoursTaken { get; set; }
+        public LeaveType Type { get; set; }
+
+        //public ScheduledLeave()
+        //{
+        //}
+
+        //public ScheduledLeave(DateTime startDate, DateTime endDate, double hours, LeaveType type)
+        //{
+        //    if (startDate > endDate)
+        //    {
+        //        throw new ArgumentOutOfRangeException(nameof(startDate));
+        //    }
+        //    StartDate = startDate;
+        //    EndDate = endDate;
+
+        //    if (hours < 0)
+        //    {
+        //        throw new ArgumentOutOfRangeException(nameof(hours));
+        //    }
+
+        //    HoursTaken = hours;
+        //    Type = type;
+        //}
+    }
+}
