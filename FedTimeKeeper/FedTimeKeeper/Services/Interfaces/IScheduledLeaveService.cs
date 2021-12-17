540fe7121c24886f8a260b422e3290f158eff7d9
Scotty <mhanson675@gmail.com>
2021-12-17 12:09:07 -0600

    Added Documentation to ScheduledLeaveService and Interface

--- FedTimeKeeper/FedTimeKeeper/Services/Interfaces/IScheduledLeaveService.cs
+++ FedTimeKeeper/FedTimeKeeper/Services/Interfaces/IScheduledLeaveService.cs
@@ -5,13 +5,34 @@ using System.Text;
 
 namespace FedTimeKeeper.Services.Interfaces
 {
+    /// <summary>
+    /// CRUD operations for Scheduled Leave entries in the local database instance.
+    /// </summary>
     public interface IScheduledLeaveService
     {
+        /// <summary>
+        /// Gets all the ScheduledLeave entries
+        /// </summary>
+        /// <returns>A list of ScheduledLeave entries</returns>
         IEnumerable<ScheduledLeave> GetAllScheduled();
 
+        /// <summary>
+        /// Gets all of the ScheduledLeave entries from the database who's EndDates have already passed based on the provided date.
+        /// </summary>
+        /// <param name="date">The date to check against</param>
+        /// <returns>A list of ScheduledLeave entries who's EndDates occur prior to the given date</returns>
         IEnumerable<ScheduledLeave> GetPastScheduled(DateTime date);
 
+        /// <summary>
+        /// Deletes the ScheduledLeave entry from the database if it exists.
+        /// </summary>
+        /// <param name="leave">The ScheduleLeave entry to delete.</param>
         void DeleteLeave(ScheduledLeave leave);
+        
+        /// <summary>
+        /// Saves the ScheduledLeave instance.
+        /// </summary>
+        /// <param name="leave">The ScheduledLeave entry to save</param>
         void SaveLeave(ScheduledLeave leave);
     }
 }
--- FedTimeKeeper/FedTimeKeeper/Services/ScheduledLeaveService.cs
+++ FedTimeKeeper/FedTimeKeeper/Services/ScheduledLeaveService.cs
@@ -9,40 +9,52 @@ using System.Threading.Tasks;
 
 namespace FedTimeKeeper.Services
 {
+    /// <summary>
+    /// CRUD operations for Scheduled Leave entries in the local database instance.
+    /// </summary>
     public class ScheduledLeaveService : IScheduledLeaveService
     {
         private readonly ILocalDatabase database;
+
 
         public ScheduledLeaveService(ILocalDatabase database)
         {
             this.database = database;
         }
 
-
+        /// <summary>
+        /// Updates the ScheduledLeave instance if it exists; otherwise creates an entry and saves it.
+        /// </summary>
+        /// <param name="leave">The ScheduledLeave entry to save</param>
         public void SaveLeave(ScheduledLeave leave)
         {
-            if (leave?.Id == 0)
-            {
-                _ = database.AddLeave(leave);
-            }
-            else
-            {
-                _ = database.UpdateLeave(leave);
-            }
+            _ = leave?.Id == 0 ? database.AddLeave(leave) : database.UpdateLeave(leave);
         }
 
+        /// <summary>
+        /// Gets all the ScheduledLeave entries from the database
+        /// </summary>
+        /// <returns>A list of ScheduledLeave entries</returns>
         public IEnumerable<ScheduledLeave> GetAllScheduled()
         {
             return database.GetAllLeaves().OrderBy(l => l.StartDate);
         }
 
+        /// <summary>
+        /// Gets all of the ScheduledLeave entries from the database who's EndDates have already passed based on the provided date.
+        /// </summary>
+        /// <param name="date">The date to check against</param>
+        /// <returns>A list of ScheduledLeave entries who's EndDates occur prior to the given date</returns>
         public IEnumerable<ScheduledLeave> GetPastScheduled(DateTime date)
         {
             IOrderedEnumerable<ScheduledLeave> pastLeaves = database.GetAllLeaves().Where(l => l.EndDate < date).OrderBy(l => l.StartDate);
 
             return pastLeaves;
         }
-
+        /// <summary>
+        /// Deletes the ScheduledLeave entry from the database if it exists.
+        /// </summary>
+        /// <param name="leave">The ScheduleLeave entry to delete.</param>
         public void DeleteLeave(ScheduledLeave leave)
         {
             _ = database.DeleteLeave(leave);
